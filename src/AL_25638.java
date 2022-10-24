bc873e8846160c4de737d78ec7974e300105ac45
Bigfish4tim <bigfish.4tim@gmail.com>
2022-10-24 10:19:00 +0900

    10-24

--- src/AL_25638.java
+++ src/AL_25638.java
@@ -7,18 +7,11 @@ public class AL_25638 {
     static void qDFS(int start, int nodeIndex, int[][] graph, boolean[] visit, int u, boolean uTrig) {
         visit[nodeIndex-1] = true;
         boolean subTrig = false;
-//
-//        if(uTrig) {
-//            subTrig = true;
-//        }
 
         for(int i=0; i<graph[nodeIndex-1].length; i++) {
             if(!visit[i] && graph[nodeIndex-1][i]==1) {
                 boolean[] visitClone = visit.clone();
-//                if(i == u-1) {
-//                    uTrig = true;
-//                }
-//                subTrig = uTrig;
+
                 if(uTrig) {
                     if(colors[start-1] != colors[i]) {
                         sum++;
@@ -29,26 +22,22 @@ public class AL_25638 {
                 } else {
                     qDFS(start, i+1, graph, visitClone, u, i == u - 1);
                 }
-//                qDFS(start, i+1, graph, visitClone, u, subTrig);
             }
         }
     }
 
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-//        int gCount = sc.nextInt();
-        int gCount = 5;
+        int gCount = sc.nextInt();
 
         colors = new int[gCount];
 
         boolean[] visit = new boolean[gCount];
 
-//        for(int i=0; i<gCount; i++) {
-//            colors[i] = sc.nextInt();
-//        }
-
-        colors = new int[]{1, 1, 1, 0, 0};
+        for(int i=0; i<gCount; i++) {
+            colors[i] = sc.nextInt();
+        }
 
         int[][] map = new int[gCount][gCount];
 
@@ -56,33 +45,29 @@ public class AL_25638 {
             Arrays.fill(map[i], 0);
         }
 
-//        for(int i=0; i<gCount-1; i++) {
-//            int x = sc.nextInt();
-//            int y = sc.nextInt();
-//
-//            map[x][y] = 1;
-//            map[y][x] = 1;
-//        }
+        for(int i=0; i<gCount-1; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
 
-//        int qCount = sc.nextInt();
+            map[x-1][y-1] = 1;
+            map[y-1][x-1] = 1;
+        }
 
-//        int[] querys = new int[qCount];
+        int qCount = sc.nextInt();
 
-//        for(int i=0; i<qCount; i++) {
-//            querys[i] = sc.nextInt();
-//        }
+        int[] querys = new int[qCount];
 
-        map[0][2] = 1;
-        map[2][0] = 1;
-        map[2][1] = 1;
-        map[1][2] = 1;
-        map[2][3] = 1;
-        map[3][2] = 1;
-        map[2][4] = 1;
-        map[4][2] = 1;
+        for(int i=0; i<qCount; i++) {
+            querys[i] = sc.nextInt();
+        }
 
-        qDFS(1,1, map, visit,3,false);
-
-        System.out.println(sum);
+        for (int query : querys) {
+            for (int j = 0; j < gCount; j++) {
+                boolean[] visitClone = visit.clone();
+                qDFS(j + 1, j + 1, map, visitClone, query, false);
+            }
+            System.out.println(sum/2);
+            sum = 0;
+        }
     }
 }
